290481c94ee5f0af7a46f335a22454a84b6b9d1f
linuxer <919313096@qq.com>
2017-12-14 14:18:23 +0800

    The prctice of the eleventh day.

---
+++ question371/hello.py
@@ -0,0 +1,23 @@
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+#
+# Example:
+# Given a = 1 and b = 2, return 3.
+#
+# Credits:
+# Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+
+
+# 一种解决方案
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MAX_INT = 0x7FFFFFFF
+        MIN_INT = 0x80000000
+        MASK = 0x100000000
+        while b:
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
