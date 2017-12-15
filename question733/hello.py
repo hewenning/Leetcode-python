6c599c61af213ecf9ec7ad952e59bffc322fd16c
linuxer <919313096@qq.com>
2017-12-15 20:53:04 +0800

    The prctice of the twelfth day.

---
+++ question733/hello.py
@@ -0,0 +1,76 @@
+#  An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+#
+# Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+#
+# To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+#
+# At the end, return the modified image.
+#
+# Example 1:
+#
+# Input:
+# image = [[1,1,1],[1,1,0],[1,0,1]]
+# sr = 1, sc = 1, newColor = 2
+# Output: [[2,2,2],[2,2,0],[2,0,1]]
+# Explanation:
+# From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected
+# by a path of the same color as the starting pixel are colored with the new color.
+# Note the bottom corner is not colored 2, because it is not 4-directionally connected
+# to the starting pixel.
+#
+# Note:
+# The length of image and image[0] will be in the range [1, 50].
+# The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+# The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+
+
+# 一种解决方案
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        rows, cols, orig_color = len(image), len(image[0]), image[sr][sc]
+        def traverse(row, col):
+            if (not (0 <= row < rows and 0 <= col < cols)) or image[row][col] != orig_color:
+                return
+            image[row][col] = newColor
+            [traverse(row + x, col + y) for (x, y) in ((0, 1), (1, 0), (0, -1), (-1, 0))]
+        if orig_color != newColor:
+            traverse(sr, sc)
+        return image
+
+
+
+# 另一种解决方案
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        def flood(sr, sc):
+            image[sr][sc] = newColor
+            if sr-1 >= 0 and image[sr-1][sc] == oldColor:
+                flood(sr-1, sc)
+            if sc-1 >= 0 and image[sr][sc-1] == oldColor:
+                flood(sr, sc-1)
+            if sr+1 < len(image) and image[sr+1][sc] == oldColor:
+                flood(sr+1, sc)
+            if sc+1 < len(image[0]) and image[sr][sc+1] == oldColor:
+                flood(sr, sc+1)
+
+        if image[sr][sc] == newColor:
+            return image
+
+        oldColor = image[sr][sc]
+        flood(sr, sc)
+
+        return image
