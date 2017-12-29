0d71262944af20b121a410ba63c089f94bffda87
linuxer <919313096@qq.com>
2017-12-29 10:31:56 +0800

    The practice of the nighteen day.

---
+++ question13/hello.py
@@ -0,0 +1,17 @@
+# Given a roman numeral, convert it to an integer.
+#
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
+# 一种解决方案
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+        res, p = 0, 'I'
+        for c in s[::-1]:
+            res, p = res - d[c] if d[c] < d[p] else res + d[c], c
+        return res
