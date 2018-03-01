1da354a0dfe8eeedac2f920a64388f0f2993b116
linuxer <919313096@qq.com>
2018-03-01 11:59:12 +0800

    The practice of thirty-first day.

---
+++ question118/hello.py
@@ -0,0 +1,26 @@
+# Given numRows, generate the first numRows of Pascal's triangle.
+#
+# For example, given numRows = 5,
+# Return
+#
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+
+# 一种解决方案
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        pascal = [[1]*(i+1) for i in range(numRows)]
+        for i in range(numRows):
+            for j in range(1,i):
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+        return pascal
