bf02e8ca9d22b2386e839b318128b706ac68030d
unknown <919313096@qq.com>
2018-03-07 15:00:10 +0800

    The practice of thirty-sixth day.

--- question643/hello.py
+++ question643/hello.py
@@ -10,3 +10,12 @@
 
 
 # 一种解决方案
+class Solution:
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        sums = [0] + list(itertools.accumulate(nums))
+        return max(map(operator.sub, sums[k:], sums)) / k
