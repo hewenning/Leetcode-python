95e7bf131d52f5f063fe30321e57a43046be7f0b
hewenning <919313096@qq.com>
2018-03-10 12:47:57 +0800

    The practice of thirty-ninth day.

---
+++ question9/hello.py
@@ -0,0 +1,51 @@
+# Determine whether an integer is a palindrome. Do this without extra space.
+#
+# click to show spoilers.
+#
+# Some hints:
+# Could negative integers be palindromes? (ie, -1)
+#
+# If you are thinking of converting the integer to string, note the restriction of using extra space.
+#
+# You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+#
+# There is a more generic way of solving this problem.
+
+
+# 一种解决方案
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        ranger = 1
+        while x / ranger >= 10:
+            ranger *= 10
+
+        while x:
+            left = x / ranger
+            right = x % 10
+            if left != right:
+                return False
+
+            x = (x % ranger) / 10
+            ranger /= 100
+
+        return True
+
+
+# 另外一种解决方案
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            return int(str(x)[::-1]) == x
