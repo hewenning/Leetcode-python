c8d8e538a6a552d538ab7723dbae6513f251efc9
hewenning <919313096@qq.com>
2018-03-13 15:19:30 +0800

    The practice of fouty-second day.

---
+++ question234/hello.py
@@ -0,0 +1,46 @@
+# Given a singly linked list, determine if it is a palindrome.
+#
+# Follow up:
+# Could you do it in O(n) time and O(1) space?
+
+
+# 一种解决方案
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        rev = None
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, head = head, rev, head.next
+        tail = head.next if fast else head
+        isPali = True
+        while rev:
+            isPali = isPali and rev.val == tail.val
+            head, head.next, rev = rev, head, rev.next
+            tail = tail.next
+        return isPali
+
+
+# 另外一种解决方案
+def isPalindrome(self, head):
+    rev = None
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        rev, rev.next, slow = slow, rev, slow.next
+    if fast:
+        slow = slow.next
+    while rev and rev.val == slow.val:
+        slow = slow.next
+        rev = rev.next
+    return not rev
