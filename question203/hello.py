f3b78f3a9239cb98389bed55cb528b73d6ab286c
hewenning <919313096@qq.com>
2018-03-13 15:24:37 +0800

    The practice of fouty-second day.

---
+++ question203/hello.py
@@ -0,0 +1,35 @@
+# Remove all elements from a linked list of integers that have value val.
+#
+# Example
+# Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+# Return: 1 --> 2 --> 3 --> 4 --> 5
+#
+# Credits:
+# Special thanks to @mithmatt for adding this problem and creating all test cases.
+
+
+# 一种解决方案
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        dummy.next = head
+        next = dummy
+
+        while next != None and next.next != None:
+            if next.next.val == val:
+                next.next = next.next.next
+            else:
+                next = next.next
+
+        return dummy.next
