e6fbd299dc2a2892e21bce99718ec70d807b91a5
hewenning <919313096@qq.com>
2018-03-17 09:16:38 +0800

    The practice of forty-sixth day.

---
+++ question14/hello.py
@@ -0,0 +1,18 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+
+# 一种解决方案
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        for i, letter_group in enumerate(zip(*strs)):
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
