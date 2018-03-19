001400daf2303c31c094f77ed6c75111da3a48ee
hewenning <919313096@qq.com>
2018-03-19 13:30:59 +0800

    The practice of forty-eighth day.

---
+++ question190/hello.py
@@ -0,0 +1,20 @@
+# Reverse bits of a given 32 bits unsigned integer.
+#
+# For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+#
+# Follow up:
+# If this function is called many times, how would you optimize it?
+#
+# Related problem: Reverse Integer
+
+
+# 一种解决方案
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for _ in range(32):
+            res = (res<<1) + (n&1)
+            n>>=1
+        return res
