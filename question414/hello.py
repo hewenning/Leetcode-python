6c33ca1ebf37cf899cd7b28716d2e922f1db6fd9
hewenning <919313096@qq.com>
2018-03-20 13:33:31 +0800

    The pracitce of forty-ninth day.

--- question414/hello.py
+++ question414/hello.py
@@ -22,3 +22,18 @@
 
 
 # 一种解决方案
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        one = two = three = -sys.maxint
+        for i in nums:
+            if i > one:
+                one, two, three = i, one, two
+            elif i > two and i < one:
+                two, three = i, two
+            elif i > three and i < two:
+                three = i
+        return three if three != -sys.maxint else one
