c3455ab5fd7e47042984dbb97f370b071fb144fa
hewenning <919313096@qq.com>
2018-03-22 08:44:56 +0800

    The practice of fifty-first day.

--- question189/hello.py
+++ question189/hello.py
@@ -11,3 +11,12 @@
 
 
 # 一种解决方案
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
