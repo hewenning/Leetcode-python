ccc7dcf765181d22b8be37180b978af4c5c565f3
hewenning <919313096@qq.com>
2018-03-23 08:45:07 +0800

    The practice of fifty-second day.

--- question7/hello.py
+++ question7/hello.py
@@ -17,3 +17,12 @@
 
 
 # 一种解决方案
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        s = (x > 0) - (x < 0)
+        r = int(str(x*s)[::-1])
+        return s*r * (r < 2**31)
