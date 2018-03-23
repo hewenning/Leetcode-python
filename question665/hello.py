63a39f12f268bc7a9d30f8672246984599e3ba85
hewenning <919313096@qq.com>
2018-03-23 08:49:30 +0800

    The practice of fifty-second day.

--- question665/hello.py
+++ question665/hello.py
@@ -14,3 +14,16 @@
 
 
 # 一种解决方案
+class Solution:
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        one, two = nums[:], nums[:]
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                one[i] = nums[i + 1]
+                two[i + 1] = nums[i]
+                break
+        return one == sorted(one) or two == sorted(two)
