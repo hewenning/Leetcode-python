43e267da9e4c982689781fa80ff9239c00dfb10f
hewenning <919313096@qq.com>
2018-03-23 08:44:59 +0800

    The practice of fifty-second day.

--- question7/hello.py
+++ question7/hello.py
@@ -16,4 +16,4 @@
 # Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
 
-# 
+# 一种解决方案
