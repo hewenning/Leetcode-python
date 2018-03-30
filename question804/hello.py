966f2652d998d483555b08b834c6d322e85dabe9
hewenning <919313096@qq.com>
2018-03-30 18:33:15 +0800

    The practice of third day in leetcode.

--- question804/hello.py
+++ question804/hello.py
@@ -48,3 +48,12 @@
 
 
 # 一種解決方案
+class Solution:
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        d = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+             "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+        return len({''.join(d[ord(i) - ord('a')] for i in w) for w in words})
