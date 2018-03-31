7d2b4302cd96fdc3c0ddeb06b13bdc7f9952ef94
hewenning <919313096@qq.com>
2018-03-31 09:33:02 +0800

    The practice of forth day in leetcode-cn.

--- question78/hello.py
+++ question78/hello.py
@@ -31,3 +31,19 @@ class Solution:
         res.append(path)
         for i in range(index, len(nums)):
             self.dfs(nums, i + 1, path + [nums[i]], res)
+
+
+# 另外一種解決方案
+def subsets2(self, nums):
+    res = []
+    nums.sort()
+    for i in range(1<<len(nums)):
+        tmp = []
+        for j in range(len(nums)):
+            if i & 1 << j:  # if i >> j & 1:
+                tmp.append(nums[j])
+        res.append(tmp)
+    return res
+
+
+# 另外一種解決方案
