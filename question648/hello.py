9848ccd77ac677f640e8e277887ad5855eb2cc6c
hewenning <919313096@qq.com>
2018-04-02 13:51:57 +0800

    The practice of sixth day in leetcode-cn.

--- question648/hello.py
+++ question648/hello.py
@@ -38,3 +38,29 @@ class Solution:
 
 
 # 另外一種解決方案
+class Solution:
+    def replaceWords(self, roots, sentence):
+        """
+        :type dict: List[str]
+        :type sentence: str
+        :rtype: str
+        """
+        _trie = lambda: collections.defaultdict(_trie)
+        trie = _trie()
+        END = True
+        for root in roots:
+            cur = trie
+            for letter in root:
+                cur = cur[letter]
+            cur[END] = root
+
+        def replace(word):
+            cur = trie
+            for letter in word:
+                if letter not in cur: break
+                cur = cur[letter]
+                if END in cur:
+                    return cur[END]
+            return word
+
+        return " ".join(map(replace, sentence.split()))
