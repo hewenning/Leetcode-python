b6bb0580af9b732c4aea2e05e2351b74783b06e6
hewenning <919313096@qq.com>
2018-04-04 13:18:44 +0800

    The practice of eighth day in leetcode-cn.

--- question647/hello.py
+++ question647/hello.py
@@ -18,3 +18,19 @@
 
 
 # 一种解决方案
+class Solution(object):
+    def countSubstrings(self, S):
+        """
+        :type s: str
+        :rtype: int
+        """
+        N = len(S)
+        ans = 0
+        for center in xrange(2*N - 1):
+            left = center / 2
+            right = left + center % 2
+            while left >= 0 and right < N and S[left] == S[right]:
+                ans += 1
+                left -= 1
+                right += 1
+        return ans
