71dd321f92d80b508b11c3f2fa0c86924ba58c2a
hewenning <919313096@qq.com>
2018-04-05 11:16:34 +0800

    The practice of ninth day in leetcode-cn.

--- question46/hello.py
+++ question46/hello.py
@@ -20,3 +20,6 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        return [[n] + p
+            for i, n in enumerate(nums)
+            for p in self.permute(nums[:i] + nums[i+1:])] or [[]]
