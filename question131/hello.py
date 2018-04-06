8698739de4af81542b895231044d1c327136b15b
hewenning <919313096@qq.com>
2018-04-06 10:37:14 +0800

    The practice of tenth day in leetcode-cn.

--- question131/hello.py
+++ question131/hello.py
@@ -12,3 +12,13 @@
 
 
 # 一種解決方案
+class Solution:
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        return [[s[:i]] + rest
+            for i in range(1, len(s)+1)
+            if s[:i] == s[i-1::-1]
+            for rest in self.partition(s[i:])] or [[]]
