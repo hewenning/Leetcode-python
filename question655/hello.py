9202c2690d6bd5681dbd85a94a29abbd6d045482
hewenning <919313096@qq.com>
2018-04-07 13:41:15 +0800

    The practice of eleventh day in leetcode-cn.

--- question655/hello.py
+++ question655/hello.py
@@ -44,3 +44,25 @@
 
 
 # 一種解決方案
+class Solution:
+    def printTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[str]]
+        """
+        if not root: return [""]
+
+        def depth(root):
+            if not root: return 0
+            return max(depth(root.left), depth(root.right)) + 1
+
+        d = depth(root)
+        self.res = [[""] * (2 ** d - 1) for _ in range(d)]
+
+        def helper(node, d, pos):
+            self.res[-d - 1][pos] = str(node.val)
+            if node.left: helper(node.left, d - 1, pos - 2 ** (d - 1))
+            if node.right: helper(node.right, d - 1, pos + 2 ** (d - 1))
+
+        helper(root, d - 1, 2 ** (d - 1) - 1)
+        return self.res
