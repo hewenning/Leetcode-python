8aca0d0ccadb0a3837043c11ee4d075be9655ee5
hewenning <919313096@qq.com>
2018-04-08 17:02:37 +0800

    The practice of twelfth day in leetcode-cn.

--- question813/hello.py
+++ question813/hello.py
@@ -88,3 +88,25 @@
 
 
 # 一種解決方案
+class Solution:
+    def largestSumOfAverages(self, a, k):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: float
+        """
+        def rec(st, k):
+            if (st, k) in cache:
+                return cache[(st, k)]
+            if k == 1:
+                cache[(st, k)] = sum(a[st:])/(len(a)-st)
+                return cache[(st, k)]
+            total = 0
+            res = -math.inf
+            for i in range(st, len(a)-k+1):
+                total += a[i]
+                res = max(res, (total/(i-st+1)) + rec(i+1, k-1))
+            cache[(st, k)] = res
+            return cache[(st, k)]
+        cache = {}
+        return rec(0, k)
