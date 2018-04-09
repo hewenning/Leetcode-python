34cb6833af34b09a7dc3a44187b6036c8d2bc05b
hewenning <919313096@qq.com>
2018-04-09 12:59:01 +0800

    The practice of thirteenth day in leetcode-cn.

--- question508/hello.py
+++ question508/hello.py
@@ -18,3 +18,28 @@
 
 
 # 一種解決方案
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findFrequentTreeSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root == None: return []
+
+        def getSum(node):
+            if node == None: return 0
+            s = node.val + getSum(node.left) + getSum(node.right)
+            c[s] += 1
+            return s
+
+        c = collections.Counter()
+        getSum(root)
+        frequent = max(c.values())
+        return [s for s in c.keys() if c[s] == frequent]
