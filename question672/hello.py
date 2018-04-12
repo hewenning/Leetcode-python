f4d2648a445abf1ee066cb1993a2555d9dbef96a
hewenning <919313096@qq.com>
2018-04-12 13:57:08 +0800

    The practice of fifteenth day in leetcode-cn.

--- question672/hello.py
+++ question672/hello.py
@@ -21,3 +21,24 @@
 
 
 # 一种解决方案
+class Solution:
+    def flipLights(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        seen = set()
+        for cand in itertools.product((0, 1), repeat = 4):
+            if sum(cand) % 2 == m % 2 and sum(cand) <= m:
+                A = []
+                for i in range(min(n, 3)):
+                    light = 1
+                    light ^= cand[0]
+                    light ^= cand[1] and i % 2
+                    light ^= cand[2] and i % 2 == 0
+                    light ^= cand[3] and i % 3 == 0
+                    A.append(light)
+                seen.add(tuple(A))
+
+        return len(seen)
